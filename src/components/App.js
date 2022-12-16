1e8781f6612f3fb6f860b48da6a332de2d37438f
Salma Shaik <111558265+SalmaRivaan@users.noreply.github.com>
2022-12-16 21:17:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,32 @@
 import React, { useState } from "react";
 import "../styles/App.css";
-
 const App = () => {
-  const [data, setData] = useState(true);
+  const [welcomepart, setWelcomepart] = useState(true);
+  const [aboutpart, setAboutpart] = useState(false);
+  const togglethis = () => {
+    setWelcomepart(!welcomepart);
+    setAboutpart(!aboutpart);
+  }
   return (
     <div id="main">
-      {data ? (
-        <div id="welcome-div">
-          <h1>Welcome to Newton School!!</h1>
-        </div>
-      ) : (
-        <div id="about-div">
-          <h1>
-            Newton School is a neo-university providing highly immersive tech
-            learning to millions of students, to tap into new-age tech
-            opportunities.
-          </h1>
-        </div>
-      )}
-      <button id="toggle" onClick={() => setData(!data)}>
-        Toggle
-      </button>
+      {
+        welcomepart ?
+          <div id="welcome-div">
+            <h1>Welcome to Newton School!!</h1>
+          </div> : null
+      }
+      {
+        aboutpart ?
+          <div id="about-div">
+            <h1>
+              Newton School is a neo-university providing highly immersive tech
+              learning to millions of students, to tap into new-age tech
+              opportunities.
+            </h1>
+          </div> : null
+      }
+      <button id="toggle" onClick={togglethis}>Toggle</button>
     </div>
   );
 };
-
 export default App;
